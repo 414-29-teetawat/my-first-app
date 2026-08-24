a2aaadd2abb2124804f588f118bca1430f995e27
414-29-teetawat <56420@yupparaj.ac.th>
2026-08-24 14:22:39 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,6 +101,6 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นายธีร์ธวัช เพ็ชรพงษ์ 4/14 เลขที่29")
 
 
